3d0038f0fee840306c014c7ea3d6f4772a7650d6
kohshi54 <kohshi54.yam@gmail.com>
2024-06-30 02:23:28 +0900

    user numpy ndarray for speed

--- 8.py
+++ 8.py
@@ -1,7 +1,5 @@
 import networkx as nx
-
-with open('aspath.list', 'r') as file:
-	lines = file.readlines()
+import numpy as np
 
 def print_path(path):
 	for asn in path:
@@ -21,6 +19,9 @@ def sum_matrix(mx):
 			sum += elem
 	return sum
 
+def sum_matrix_fast(mx):
+	return np.sum(mx)
+
 def hop_avg(mx, mxvpn):
     gapsum = 0
     hopsum = 0
@@ -37,6 +38,19 @@ def hop_avg(mx, mxvpn):
     print(f"hopavg: {hopsum / elemnum}")
     print(f"hopavgvpn: {hopsumvpn / elemnum}")
     print(f"gapavg: {gapsum / elemnum}")
+
+def hop_avg_fast(mx, mxvpn):
+	gapsum = np.sum(mxvpn - mx)
+	hopsum = np.sum(mx)
+	hopsumvpn = np.sum(mxvpn)
+	elemnum = mx.size
+	print(f"hopavg: {hopsum / elemnum}")
+	print(f"hopavgvpn: {hopsumvpn / elemnum}")
+	print(f"gapavg: {gapsum / elemnum}")
+
+
+with open('aspath.list', 'r') as file:
+	lines = file.readlines()
 
 print("extracte aspath")
 as_paths = []
@@ -71,6 +85,14 @@ for path in as_paths:
 		cur = path[i + 1]
 		G.add_edge(prev, cur)
 
+"""
+print(G.edges)
+pos = nx.spring_layout(G)
+nx.draw_networkx(G, pos, with_labels=True, alpha=0.5)
+plt.axis("off")
+plt.show()
+"""
+
 print("user")
 # user asn list
 userasns = []
@@ -94,14 +116,20 @@ with open('serverasn.list', 'r') as servers:
 		serverrates.append(float(rate))
 
 # cal weight user <-> server (gravity model)
+# user ndarray for speed
+"""
 weightmx = [[0 for _ in range(len(serverasns))] for _ in range(len(userasns))]
 for i in range(len(userasns)):
 	for j in range(len(serverasns)):
 		weightmx[i][j] = userrates[i] * serverrates[j]
-
 gravitymx = [[0 for _ in range(len(serverasns))] for _ in range(len(userasns))]
+"""
+userrates = np.array(userrates)
+serverrates = np.array(serverraets)
+weightmx = np.outer(userrates, serverrates)
+gravitymx = np.zeros_like(weightmx)
 
-print("hops")
+print("hops no vpn")
 """
 # get hops
 if nx.has_path(G, 1234, 5678):
@@ -109,6 +137,7 @@ if nx.has_path(G, 1234, 5678):
 """
 # get hops
 # no vpn
+"""
 hopmx = [[0 for _ in range(len(serverasns))] for _ in range(len(userasns))]
 for i in range(len(userasns)):
 	for j in range(len(serverasns)):
@@ -116,11 +145,21 @@ for i in range(len(userasns)):
 			if nx.has_path(G, userasns[i], serverasns[j]):
 				hopmx[i][j] = nx.shortest_path_length(G, source=userasns[i], target=serverasns[j])
 				gravitymx[i][j] = hopmx[i][j] * weightmx[i][j]
+"""
+hopmx = np.zeros_like(weightmx)
+for i, userasn in enumerate(userasns):
+	for j, serverasn in enumerate(serverasns):
+		if userasn in G and serverasn in G:
+			if nx.has_path(G, userasn, serverasn):
+				hopmx[i][j] = nx.shortest_path_length(G, source=userasn, target=serverasn)
+				gravitymx[i][j] = hopmx[i][j] * weightmx[i][j]
 
 #print_matrix(hopmx)
-print(f"hopsum: {sum_matrix(hopmx)}")
+#print(f"hopsum: {sum_matrix(hopmx)}")
 
 # with vpn
+print("hops with vpn")
+"""
 hopmxvpn = [[0 for _ in range(len(serverasns))] for _ in range(len(userasns))]
 for i in range(len(userasns)):
 	if userasns[i] in G:
@@ -131,9 +170,23 @@ for i in range(len(userasns)):
 					vpn2s = nx.shortest_path_length(G, source=59103, target=serverasns[j])
 					hopmxvpn[i][j] = c2vpn + vpn2s
 					gravitymx = hopmxvpn[i][j] * weightmx[i][j]
+"""
+vpnasn = 59103
+hopmxvpn = np.zeros_like(weightmx)
+for i, userasn in enumerate(userasns):
+	if userasn in G:
+		if nx.has_path(G, userasn, vpnasn):
+			c2vpn = nx.shortest_path_length(G, source=userasn, target=vpnasn)
+			for j, serverasn in enumerate(serverasns):
+				if serverasn in G:
+					if nx.has_path(G, vpnasn, serverasn):
+						vpn2s = nx.shortest_path_length(G, source=vpnasn, target=serverasn)
+						hopmxvpn[i][j] = c2vpn + vpn2s
+						gravitymx = hopmxvpn[i][j] * weightmx[i][j]
 
 #print_matrix(hopmxvpn)
-print(f"hopsumvpn: {sum_matrix(hopmxvpn)}")
+#print(f"hopsumvpn: {sum_matrix(hopmxvpn)}")
 
-hop_avg(hopmx, hopmxvpn)
-
+print("cal")
+hop_avg_fast(hopmx, hopmxvpn)
+print(f"gravitycost: {sum_matrix_fast(gravitymx)}")
