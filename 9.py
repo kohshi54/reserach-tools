716baf98b058438875e468266f8bfa4442e63267
kohshi54 <kohshi54.yam@gmail.com>
2024-07-01 22:17:44 +0900

    reverse to get expected resutl

--- 9.py
+++ 9.py
@@ -159,7 +159,7 @@ def main():
 	# use ndarray for speed
 	userrates = np.array(userrates)
 	serverrates = np.array(serverrates)
-	gravity = np.outer(userrates, serverrates)
+	gravity = np.outer(serverrates, userrates)
 
 	# get hops
 	print("hops")
