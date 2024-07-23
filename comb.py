51b63007a5a84e68ee12621564716158c6b06d05
kohshi54 <kohshi54.yam@gmail.com>
2024-07-23 13:52:08 +0900

    fix error (c2v,v2s have to pick the same v when cal)

--- comb.py
+++ comb.py
@@ -2,6 +2,7 @@ import sys
 import networkx as nx
 #import numpy as np
 import cupy as np
+import multiprocessing as mp
 
 """ 2^50 oom """
 def create_all_combination(nodes):
@@ -17,21 +18,60 @@ def create_all_combination(nodes):
 
 def calculate_avg_path_length(G, userasns, serverasns, gravity, comb):
 	print(comb)
-	#if comb:
-		#hopmx = calculate_hops_with_vpns(G, userasns, serverasns, comb)
-	#else:
-	hopmx = calculate_hops_with_vpns(G, userasns, serverasns, comb)
+	if comb:
+		hopmx = calculate_hops_with_vpns(G, userasns, serverasns, comb)
+	else:
+		return (0,0)
+		#hopmx = calculate_direct_path_length(G, userasns, serverasns)
 	costmxvpn = hopmx * gravity
+	"""
 	hopsumvpn = np.sum(hopmx)
 	elemnum = hopmx.size
 	print(f"hopavgvpn: {hopsumvpn / elemnum}")
 	print(f"gravitycostvpn: {np.sum(costmxvpn)}")
 	return (hopsumvpn/elemnum,np.sum(costmxvpn))
+	"""
+	valid_hopmx = hopmx[(hopmx != 0)]
+	valid_costmxvpn = costmxvpn[(costmxvpn != 0)]
+	hopsumvpn = np.sum(valid_hopmx)
+	valid_elemnum = valid_hopmx.size
+	gravitycostvpn = np.sum(valid_costmxvpn)
+	print(f"hopavgvpn: {hopsumvpn / valid_elemnum}")
+	#print(f"hopavgvpn: {valid_hopmx.mean()}")
+	print(f"gravitycostvpn: {gravitycostvpn}")
+	print(f"validpathrate: {valid_hopmx.size/hopmx.size} (allpath:{hopmx.size},validpath:{valid_hopmx.size})")
+	return (hopsumvpn/valid_elemnum, gravitycostvpn)
 
-def write_file(comb, length, weighted_length, outfilename):
+def write_file(comb, node_rank, length, weighted_length, outfilename):
 	with open(outfilename, 'a') as outfile:
 		nodes = ','.join(map(str, comb))
-		outfile.write(f"relay({nodes}) {length} {weighted_length}\n")
+		ranks = ','.join(map(str, node_rank))
+		outfile.write(f"relay({nodes}) relay({ranks}) {length} {weighted_length}\n")
+
+def cal_hop(userasn, G, serverasn):
+	if userasn in G and serverasn in G:
+		try:
+			return (userasn, serverasn, nx.shortest_path_length(G, source=userasn, target=serverasn))
+		except nx.NetworkXNoPath:
+			return (userasn, serverasn, None)
+	else:
+		return (userasn, serverasn, None)
+
+def parallel_shortest_path_length(userasns, G, serverasns):
+	with mp.Pool(int(mp.cpu_count()/2)) as pool:
+		tasks = []
+		for userasn in userasns:
+			for serverasn in serverasns:
+				tasks.append((userasn, G, serverasn))
+		results = pool.starmap(cal_hop, tasks)
+	
+	#results_dict = {}
+	results_dict = {userasn: {serverasn: 0 for serverasn in serverasns} for userasn in userasns}
+	for result in results:
+		userasn, serverasn, hops = result
+		results_dict[userasn][serverasn] = hops
+	
+	return results_dict
 
 def parse_aspath(aspath_file):
 	with open(aspath_file, 'r') as file:
@@ -103,32 +143,79 @@ def load_relay_nodes(vpnfile):
 			relay_nodes.append(int(node))
 	return relay_nodes
 
+def calculate_direct_path_length(G, userasns, serverasns):
+	"""
+	hopmxvpn = np.zeros((len(userasns), len(serverasns)))
+	for i,userasn in enumerate(userasns):
+		if userasn in G:
+			for j,serverasn in enumerate(serverasns):
+				if serverasn in G:
+					hopmx[i][j] = nx.shortest_path_length(G, source=userasn, target=serverasn)	
+	"""
+
+	hopmxvpn = np.zeros((len(userasns), len(serverasns)))
+	c2shops = parallel_shortest_path_length(userasns, G, serverasns)
+	hopmxvpn = np.array([
+		[
+			(c2shops[userasn][serverasn]) if userasn in G and serverasn in G else 0
+			for serverasn in serverasns
+		]
+		for userasn in userasns
+	])
+	return hopmxvpn
+
 def calculate_hops_with_vpns(G, userasns, serverasns, vpnasns):
 	hopmxvpn = np.zeros((len(userasns), len(serverasns)))
+	for i,userasn in enumerate(userasns):
+		if userasn in G:
+			for j,serverasn in enumerate(serverasns):
+				if serverasn in G:
+					min_length = float('inf')
+					for node in vpnasns:
+						try:
+							c2v_length = nx.shortest_path_length(G, source=userasn, target=node)
+							v2s_length = nx.shortest_path_length(G, source=node, target=serverasn)
+							c2s_length = c2v_length + v2s_length
+						except nx.NetworkXNoPath:
+							c2s_length = float('inf')
+						if c2s_length < min_length:
+							min_length = c2s_length
+					if min_length != float('inf'):
+						hopmxvpn[i][j] = min_length
+					else:
+						hopmxvpn[i][j] = 0
+					
+	"""
 	c2vpnhops = {}
 	for userasn in userasns:
 		if userasn in G:
-			try:
-				min_length = float('inf')
-				for node in vpnasns:
+			min_length = float('inf')
+			for node in vpnasns:
+				try:
 					cur_length = nx.shortest_path_length(G, source=userasn, target=node)
-					if (cur_length < min_length):
-						min_length = cur_length
+				except nx.NetworkXNoPath:
+					cur_length = float('inf')
+				if (cur_length < min_length):
+					min_length = cur_length
+			if min_length != float('inf'):
 				c2vpnhops[userasn] = min_length
-			except nx.NetworkXNoPath:
+			else:
 				c2vpnhops[userasn] = None
 	
 	vpn2shops = {}
 	for serverasn in serverasns:
 		if serverasn in G:
-			try:
-				min_length = float('inf')
-				for node in vpnasns:
+			min_length = float('inf')
+			for node in vpnasns:
+				try:
 					cur_length = nx.shortest_path_length(G, source=node, target=serverasn)
-					if (cur_length < min_length):
-						min_length = cur_length
+				except nx.NetworkXNoPath:
+					cur_length = float('inf')
+				if (cur_length < min_length):
+					min_length = cur_length
+			if (min_length != float('inf')):
 				vpn2shops[serverasn] = min_length
-			except nx.NetworkXNoPath:
+			else:
 				vpn2shops[serverasn] = None
 	
 	hopmxvpn = np.array([
@@ -138,6 +225,7 @@ def calculate_hops_with_vpns(G, userasns, serverasns, vpnasns):
 		]
 		for userasn in userasns
 	])
+	"""
 	"""
 	for i, userasn in enumerate(userasns):
 		for j, serverasn in enumerate(serverasns):
@@ -159,15 +247,22 @@ def main():
 	serverrates = np.array(serverrates)
 	gravity = np.outer(userrates, serverrates)
 
-	nodes = load_relay_nodes('top50.txt')
+	#length,weighted_length = calculate_avg_path_length(G, userasns, serverasns, gravity, [59103])
+	#print(f"{length}, {weighted_length}")
+
+	#"""
+	nodes = load_relay_nodes('top1.txt')
 	n = len(nodes)
 	for i in range(1 << n):
 		comb = []
+		node_rank = []
 		for j in range(n):
 			if (i & (1 << j)):
 				comb.append(nodes[j])
+				node_rank.append(j+1)
 		length,weighted_length = calculate_avg_path_length(G, userasns, serverasns, gravity, comb)
-		write_file(comb, length, weighted_length, 'outfile')
+		write_file(comb, node_rank, length, weighted_length, 'kk.outfile')
+	#"""
 
 	""" oom(2^50 set of list is
 	#nodes = (1, 2, 3, 4, 5)
